c9b0e9ab1608a5e881f0a1e7b7af552c32157a81
Ezequiel Delgadillo <ezequieldelgadillo05@gmail.com>
2025-03-23 22:44:20 -0300

    primera clase creada del ejercicio1

--- claseEjercicio1/Program.cs
+++ claseEjercicio1/Program.cs
@@ -9,4 +9,33 @@ namespace claseEjercicio1
             Console.WriteLine("Hello World!");
         }
     }
+
+
+    public class CuentaBanco
+    {
+        public string nombre;
+        public string apellido;
+        public string telefono;
+        public int dni;
+        public int cbu;
+        public int saldo;
+
+        public void ingresarDinero()
+        {
+            Console.WriteLine("Se ingreso dinero");
+
+        }
+
+        public void sacoDinero()
+        {
+            Console.WriteLine("Se saco dinero");
+        }
+
+        public void Transferencia()
+        {
+            Console.WriteLine("Se transfirio dinero");
+        }
+        
+
+    }
 }
